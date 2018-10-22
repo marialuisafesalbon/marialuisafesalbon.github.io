1191590eae0582a6b4f088190d27c540919aabb0
Maria Luisa <44377426+marialuisafesalbon@users.noreply.github.com>
2018-10-23 02:43:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,35 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-
 <?php
 
-$grade = 89;
+$grade = 55 ;
 
-if ($grade =>95) {
-    
-    echo 'Your grade is $grade and equivalent to A'
-        
-        elseif ($grade => 89) {
-            
-            echo 'Your grade is $grade and equivalent to B'
-        }
-    
-    else echo 'failed'
+if ($grade >= 95) {
+  
+  echo "Your grade is"  .$grade. "that is equivalent to A!";
+  	} 
+elseif ($grade >= 89) {
 
+  echo "Your grade is"  .$grade. "that is equivalent to B+!";
+	}
+elseif ($grade >= 83) {
 
-}
+  echo "Your grade is"  .$grade. "that is equivalent to B!";
+    }
+elseif ($grade >= 77) {
+
+  echo "Your grade is"  .$grade. "that is equivalent to C+!";
+    }
+elseif ($grade >= 65) {
+
+  echo "Your grade is"  .$grade. "that is equivalent to D+!";
+    }
+elseif ($grade >= 60) {
+
+  echo "Your grade is"  .$grade. "that is equivalent to D!";
+    }
+else {
+   
+   echo "Your grade is"  .$grade. "that is equivalent to F!";
+ 	}
 
 ?>
     
-    </body>
-</html>
