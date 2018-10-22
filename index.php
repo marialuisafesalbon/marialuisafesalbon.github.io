d16df8cb781f7192fa6b45828c4d3e7377e01f8e
Maria Luisa <44377426+marialuisafesalbon@users.noreply.github.com>
2018-10-23 02:42:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+
+$grade = 89;
+
+if ($grade =>95) {
+    
+    echo 'Your grade is $grade and equivalent to A'
+        
+        elseif ($grade => 89) {
+            
+            echo 'Your grade is $grade and equivalent to B'
+        }
+    
+    else echo 'failed'
+
+
+}
+
+?>
+    
+    </body>
+</html>
